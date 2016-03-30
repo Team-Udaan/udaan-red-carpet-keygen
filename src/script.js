5b5a58ac1772c74868ec2a507fd8fff5a46c9f75
Dixita Ganatra <dixita0607@gmail.com>
2016-03-30 18:24:57 +0530

    Sanitize Enrollment No. to have maximum of 12 digits

--- src/script.js
+++ src/script.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
 
+  function sanitize(string) {
+    return string.replace(/[\D]/g, '').substr(0, 12);
+  }
+
   $('#enroll').on('input', function () {
+    this.value = sanitize(this.value);
     $('#pin').text(
       this.value ?
         hash(this.value) :
